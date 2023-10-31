2b20ad5e213ed0a06b731d637d9b90cd07c776bb
jedaydiya <joloanjed@gmail.com>
2023-10-31 20:45:53 +0800

    Added Clerk Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from 'next/image'
-
+import { UserButton } from "@clerk/nextjs"
+import { User } from "lucide-react"
 export default function Home() {
   return (
     <>
+      <UserButton afterSignOutUrl="/" />
       <div className='text-red-300'>wyrd-kanban</div>
     </>
   )
