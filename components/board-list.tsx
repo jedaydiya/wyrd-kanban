ae55bfee5e3e1da757a528a0afea0addaa78759d
jedaydiya <joloanjed@gmail.com>
2023-11-18 15:22:07 +0800

    Added Delete Board Server Action

--- components/board-list.tsx
+++ components/board-list.tsx
@@ -10,23 +10,39 @@ import {
 
 import { formatDistance } from "date-fns";
 import { Button } from "./ui/button";
+import { deleteBoard } from "@/server-actions/actions";
 export default async function BoardList() {
   const boards = await fetchBoards();
   return (
     <>
-      <div className="mx-auto grid max-w-7xl grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+      <div className="mx-auto mt-4 grid max-w-7xl grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {boards?.map((board) => (
-          <Card className="h-[190px] text-white rounded-md border-none bg-background shadow-md shadow-accent">
+          <Card className="h-[190px] rounded-md border-none bg-background text-white shadow-md shadow-accent">
             <CardHeader>
               <CardTitle>{board.board_name}</CardTitle>
-              <CardDescription className="text-white/80">{formatDistance(board.createdAt, new Date(), {
-                addSuffix: true,
-              })}</CardDescription>
+              <CardDescription className="text-white/80">
+                {formatDistance(board.createdAt, new Date(), {
+                  addSuffix: true,
+                })}
+              </CardDescription>
             </CardHeader>
-            <CardContent className="h-[20px] truncate text-sm text-muted-foreground">{board.board_description || "No Description"}</CardContent>
+            <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
+              {board.board_description || "No Description"}
+            </CardContent>
             <CardFooter className="flex gap-2">
-              <Button variant="outline" className="w-full mt-2 text-md gap-4">View</Button>
-              <Button variant="outline" className="w-full mt-2 text-md gap-4">Delete</Button>
+              <Button variant="outline" className="text-md mt-2 w-full gap-4">
+                View
+              </Button>
+              <form action={deleteBoard} className="w-full">
+                <input type="hidden" name="boardId" value={board.id} />
+                <Button
+                  type="submit"
+                  variant="outline"
+                  className="text-md mt-2 w-full gap-4"
+                >
+                  Delete
+                </Button>
+              </form>
             </CardFooter>
           </Card>
         ))}
--- server-actions/actions.ts
+++ server-actions/actions.ts
@@ -5,7 +5,7 @@ import { boards } from "@/lib/db/schema";
 import { revalidatePath } from "next/cache";
 import { auth } from "@clerk/nextjs/server";
 import { BoardSchema } from "@/lib/types";
-import { type InferSelectModel } from "drizzle-orm";
+import { eq, type InferSelectModel } from "drizzle-orm";
 
 export type SelectBoard = InferSelectModel<typeof boards>;
 export async function createNewBoard(newBoard: unknown) {
@@ -34,6 +34,21 @@ export async function fetchBoards() {
   try {
     const boardResults: SelectBoard[] = await db.select().from(boards);
     return boardResults;
+  } catch (err) {
+    if (err instanceof Error) console.log(err.stack);
+  }
+}
+
+export async function deleteBoard(data: FormData) {
+  const boardIdValue = data.get("boardId");
+  if (boardIdValue === null || typeof boardIdValue !== "string") {
+    throw new Error("User ID is missing or not a string from FormData");
+  }
+  const boardId = parseInt(boardIdValue, 10);
+
+  try {
+    await db.delete(boards).where(eq(boards.id, boardId));
+    revalidatePath("/boards");
   } catch (err) {
     if (err instanceof Error) console.log(err.stack);
   }
